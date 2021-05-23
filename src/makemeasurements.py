def48d237abd204d372bcba6eec990a6c370e077
otsatukka <pukonoma@gmail.com>
2021-05-23 10:09:31 +0300

    all measurements

--- src/makemeasurements.py
+++ src/makemeasurements.py
@@ -1,21 +1,38 @@
 from SHT20 import SHT20
+from uFire_EC import uFire_EC
+from uFire_pH import uFire_pH
+from uFire_ORP import uFire_ORP
 
 class Measurements():
     def __init__(self):
         self.sht = SHT20(bus=1)
+        self.ec = uFire_EC(i2c_bus=1)
+        self.ph = uFire_pH(bus=1)
+        self.orp = uFire_ORP(bus=1)
         
     def measurePH(self):
-        print("PH")
+        self.ph.measurepH()
+        print("mV: " + str(ph.mV))
+        print("pH: " + str(ph.pH))
+        print("pOH: " + str(ph.pOH))
+        
     def measureEC(self):
-        print("PH")
+        self.ec.measureEC()
+        print("mS: " + str(ec.mS))
+        
     def measureORP(self):
-        print("PH")
+        self.orp.measureORP()
+        print("mV: " + str(orp.mV))
+        print("Eh: " + str(orp.Eh))
+
     def measureWaterTemp(self):
-        print("PH")
+        print("Water temp %.2f" %self.ec.measureTemp())
+        
     def measureAirTemp(self):
-        print("PH")
+        print("Air temp %.2f" %self.sht.temperature())
+        
     def measureHumidity(self):
-        print("PH")
+        print("Humidity %.2f" %self.sht.humidity())
 
 meas = Measurements()
 meas.measureEC()
