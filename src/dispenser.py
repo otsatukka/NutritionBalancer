5e342e36e034e1e9ddefda71e62766fe5f310ae9
otsatukka <pukonoma@gmail.com>
2021-05-24 07:35:31 +0300

    add control touches

--- src/dispenser.py
+++ src/dispenser.py
@@ -5,12 +5,12 @@ class Dispenser():
     pooldispensercapacity = 1.0 #l in min
     def __init__(self):
         self.pooldispenser = Motor(17, 27,pwm=False)
-        self.phdispenser = Motor(22, 10,pwm=False)
+        self.ecdispenser = Motor(22, 10,pwm=False)
         
-    def dispensePHML(amounttodispense)
-        self.phdispenser.forward()
+    def dispenseECML(amounttodispense)
+        self.ecdispenser.forward()
         sleep(60*(amounttodispense / nutrientdispensercapacity))
-        self.phdispenser.stop()
+        self.ecdispenser.stop()
         
     def dispensePoolLiters(amounttodispense)
         self.pooldispenser.forward()
--- src/nutrionbalancer.py
+++ src/nutrionbalancer.py
@@ -2,9 +2,33 @@ import time
 from dispenser import Dispenser
 from makemeasurements import Measurements
 class NutritionBalancer():
+    poolvolume = 1.0 # in liters
+    hydroponictankvolume = 10 # in liters
+    ecsolutionconcentration = 3 # salinity in mV
+    targetPh = 7.0
+    targetEc = 2.0
     
     def __init__(self):
         self.dispenser = Dispenser()
         self.measurer = Measurements() 
-    def measureValues(self):
+    
+    def control(self):
+        fillPool()
+        nutrientNeedEc, phCalibSolutionNeeded = determineNutrientNeed()
+        self.dispenser.dispenseECML(nutrientNeedEc)
         
+        
+    def fillPool(self):
+        self.dispenser.dispensePoolLiters(poolvolume*1.5) # Over fill
+        
+    def determineNutrientNeed(self):
+        nutrientNeedEc = 0
+        phCalibSolutionNeeded = 0 
+        humidity, airtemp, watertemp, orp, ph, ec = measurer.makeMeasurements()
+        if (ec < targetEc):
+            volumeFactor = hydroponictankvolume / poolvolume
+            volumetopool = poolvolume / ( ( ecsolutionconcentration - targetEc ) / ( targetEc - ec ) )
+            nutrientNeedEc = volumetopool*volumeFactor
+        if (ph < targetPh):
+            print("Calculate PH balancing")
+        return nutrientNeedEc,phCalibSolutionNeeded
